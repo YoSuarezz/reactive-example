1438bb3d7ec1d60116e2d95d843ce68db2234999
Sebastian <sebassuarez103@gmail.com>
2025-09-30 18:48:50 -0500

    Initial commit, se añaden dependencias y un ejemplo inicial

---
+++ src/main/java/co/edu/uco/reactiveexample/initializer/ReactiveWebExampleApplication.java
@@ -0,0 +1,15 @@
+package co.edu.uco.reactiveexample.initializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"co.edu.uco.reactiveexample"})
+public class ReactiveWebExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveWebExampleApplication.class, args);
+	}
+
+}
